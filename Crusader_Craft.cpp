3ddbf2384517ee456ddaac8588ba0b38a33e507f
Morgan Breth <mbreth1@gmail.com>
2020-02-10 18:30:31 -0600

    Update Crusader_Craft.cpp

--- Crusader_Craft.cpp
+++ Crusader_Craft.cpp
@@ -9,6 +9,7 @@
 #include <cstdlib>
 #include <string>
 #include <vector>
+#include <unistd.h>
 using namespace std;
 
 class game {
@@ -21,6 +22,11 @@ class game {
     void trainSkill();
     void healPlayer();
     void checkInventory();
+    void attackRAT();
+    void attackSNAKE();
+    void attackLIONHEART();
+    void saveGame();
+    void loadGame();
 
   public:
     int m_health;
@@ -29,12 +35,29 @@ class game {
     int m_mining;
     int m_herblore;
     int m_alchemy;
+    int savedHealth;
+    int savedMining;
+    int savedFletching;
+    int savedSmithing;
+    int savedAlchemy;
+    int savedHerblore;
     int getHealth() const;
     int getSkills() const;
+    int savingMenu() const;
 };
 
 inline int game::getHealth() const {
   return (m_health);
+}
+
+inline int game::savingMenu() const {
+  int savedHealth = m_health;
+  int savedMining = m_mining;
+  int savedFletching = m_fletching;
+  int savedSmithing = m_smithing;
+  int savedAlchemy = m_alchemy;
+  int savedHerblore = m_herblore;
+  return(savedSmithing);
 }
 
 inline int game::getSkills() const {
@@ -84,6 +107,150 @@ void game::checkHealth() {
   }
 }
 
+void game::attackRAT() {
+  int enemyHealth = 10;
+  char playerAttackChoice;
+  cout << "Player health: " << m_health << endl;
+  cout << "Enemy health: " << enemyHealth << endl;
+  cout << "Attack!" << endl;
+  sleep(2);
+  while (enemyHealth >= 0 || m_health >= 0) {
+    int enemyFightAction = rand() % 3 + 1;
+    cout << "What will you do? (a = attack, d = defend, r = run):" << endl;
+    cin >> playerAttackChoice;
+    if (playerAttackChoice == 'a') {
+      if (enemyFightAction == 1) {
+        cout << "Both attacks result in a 1 point health loss to both players!" << endl;
+        enemyHealth -= 1;
+        m_health -= 1;
+      } else if (enemyFightAction == 2) {
+        cout << "The RAT has blocked your attack! No damage dealt or taken!" << endl;
+      } else {
+        cout << "You hit the RAT for 2 damage as he coward with fear!" << endl;
+        enemyHealth -= 2;
+      }
+    } else if (playerAttackChoice == 'd') {
+      if (enemyFightAction == 1) {
+        cout << "You have blocked the RAT's attack!" << endl;
+      } else if (enemyFightAction == 2) {
+        cout << "Nothing happened! Both players chose defend!" << endl;
+      } else {
+        cout << "The RAT has hit you for 1 damage because you forgot to build up your defenses!" << endl;
+        m_health -= 1;
+      }
+    } else if (playerAttackChoice == 'r') {
+      cout << "You have successfully run away you coward!" << endl;
+      break;
+    }
+    if (m_health <= 0) {
+      cout << "You have been defeated by the RAT. Head to the hospital wing to heal up and fight again!" << endl;
+      healPlayer();
+      break;
+    } else if (enemyHealth <= 0) {
+      cout << "Congratulations! You defeated the RAT!!" << endl;
+      cout << "Now go heal up for another battle!" << endl;
+      healPlayer();
+      break;
+    } 
+  }
+}
+
+void game::attackSNAKE() {
+  int enemyHealth = 10;
+  char playerAttackChoice;
+  cout << "Player health: " << m_health << endl;
+  cout << "Enemy health: " << enemyHealth << endl;
+  cout << "Attack!" << endl;
+  sleep(2);
+  while (enemyHealth >= 0 || m_health >= 0) {
+    int enemyFightAction = rand() % 3 + 1;
+    cout << "What will you do? (a = attack, d = defend, r = run):" << endl;
+    cin >> playerAttackChoice;
+    if (playerAttackChoice == 'a') {
+      if (enemyFightAction == 1) {
+        cout << "Both attacks result in a 1 point health loss to both players!" << endl;
+        enemyHealth -= 1;
+        m_health -= 1;
+      } else if (enemyFightAction == 2) {
+        cout << "The SNAKE has blocked your attack! No damage dealt or taken!" << endl;
+      } else {
+        cout << "You hit the SNAKE for 2 damage as he coward with fear!" << endl;
+        enemyHealth -= 2;
+      }
+    } else if (playerAttackChoice == 'd') {
+      if (enemyFightAction == 1) {
+        cout << "You have blocked the SNAKES's attack!" << endl;
+      } else if (enemyFightAction == 2) {
+        cout << "Nothing happened! Both players chose defend!" << endl;
+      } else {
+        cout << "The SNAKE has hit you for 1 damage because you forgot to build up your defenses!" << endl;
+        m_health -= 1;
+      }
+    } else if (playerAttackChoice == 'r') {
+      cout << "You have successfully run away you coward!" << endl;
+      break;
+    }
+    if (m_health <= 0) {
+      cout << "You have been defeated by the SNAKE. Head to the hospital wing to heal up and fight again!" << endl;
+      healPlayer();
+      break;
+    } else if (enemyHealth <= 0) {
+      cout << "Congratulations! You defeated the SNAKE!!" << endl;
+      cout << "Now go heal up for another battle!" << endl;
+      healPlayer();
+      break;
+    } 
+  }
+}
+
+void game::attackLIONHEART() {
+  int enemyHealth = 10;
+  char playerAttackChoice;
+  cout << "Player health: " << m_health << endl;
+  cout << "Enemy health: " << enemyHealth << endl;
+  cout << "Attack!" << endl;
+  sleep(2);
+  while (enemyHealth >= 0 || m_health >= 0) {
+    int enemyFightAction = rand() % 3 + 1;
+    cout << "What will you do? (a = attack, d = defend, r = run):" << endl;
+    cin >> playerAttackChoice;
+    if (playerAttackChoice == 'a') {
+      if (enemyFightAction == 1) {
+        cout << "Both attacks result in a 1 point health loss to both players!" << endl;
+        enemyHealth -= 1;
+        m_health -= 1;
+      } else if (enemyFightAction == 2) {
+        cout << "The LIONHEART has blocked your attack! No damage dealt or taken!" << endl;
+      } else {
+        cout << "You hit the LIONHEART for 2 damage as he coward with fear!" << endl;
+        enemyHealth -= 2;
+      }
+    } else if (playerAttackChoice == 'd') {
+      if (enemyFightAction == 1) {
+        cout << "You have blocked the LIONHEART's attack!" << endl;
+      } else if (enemyFightAction == 2) {
+        cout << "Nothing happened! Both players chose defend!" << endl;
+      } else {
+        cout << "The LIONHEART has hit you for 1 damage because you forgot to build up your defenses!" << endl;
+        m_health -= 1;
+      }
+    } else if (playerAttackChoice == 'r') {
+      cout << "You have successfully run away you coward!" << endl;
+      break;
+    }
+    if (m_health <= 0) {
+      cout << "You have been defeated by the LIONHEART. Head to the hospital wing to heal up and fight again!" << endl;
+      healPlayer();
+      break;
+    } else if (enemyHealth <= 0) {
+      cout << "Congratulations! You defeated LIONHEART!!" << endl;
+      cout << "Now go heal up for another battle!" << endl;
+      healPlayer();
+      break;
+    } 
+  }
+}
+
 void game::attackEnemy() {
   int enemyPicker = rand() % 3 + 1;
   int enemy1_Rat = 1;
@@ -91,12 +258,14 @@ void game::attackEnemy() {
   int enemy3_Lionheart = 3;
   if (enemyPicker == 1) {
     cout << "Enemy is Rat!" << endl;
+    attackRAT();
   } else if(enemyPicker == 2) {
     cout << "Enemy is Snake!" << endl;
+    attackSNAKE();
   } else {
     cout << "Enemy is Lionheart!" << endl;
+    attackLIONHEART();
   }
-  cout << "Attack!" << endl;
 }
 
 void game::forgeItem() {
@@ -105,8 +274,8 @@ void game::forgeItem() {
     cout << "You are not able to smith anything at this time.\nCome back later when you are level " << smithing + 1 << endl;
   } else if (smithing < 5 && smithing > 0) {
     cout << "You are able to make bronze daggers!" << endl;
-  } else {
-    // Do nothing
+  } else if (smithing >= 5 && smithing < 10) {
+    cout << "You are able to make iron daggers!" << endl;
   }
 }
 
@@ -156,6 +325,7 @@ void game::healPlayer() {
     cout << "Press 'H' to heal player." << endl;
     cin >> heal;
     cout << "Your health is now at 10!" << endl;
+    m_health = 10;
   } else if (health >= 10) {
     cout << "You are healthy and do not need healing.\n" << endl;
   }
@@ -175,6 +345,22 @@ void game::checkInventory() {
   }
 }
 
+void game::saveGame() {
+  char userKey;
+  cout << "Press 'S' to save your game" << endl;
+  cin >> userKey;
+  savingMenu();
+  sleep(3);
+  cout << "Game saved!" << endl;
+  
+}
+
+void game::loadGame() {
+  cout << "Your previous SMITHING LEVEL is " << savedSmithing << endl;
+  cout << "Your previous MINING LEVEL is " << savedMining << endl;
+  cout << "Your previous HEALTH LEVEL is " << savedHealth << endl;
+}
+
 int main() {
   game Crusader_Craft;
   cout << "Welcome to the World of Crusader-Craft!!\n\n" << endl;
@@ -188,6 +374,8 @@ int main() {
     cout << "5. Train a skill" << endl;
     cout << "6. Heal player" << endl;
     cout << "7. Check inventory" << endl;
+    cout << "8. Save game" << endl;
+    cout << "9. Load game" << endl;
     cin >> choice; 
     switch(choice) {
       case 1:
@@ -218,6 +406,14 @@ int main() {
 
       case 7:
         Crusader_Craft.checkInventory();
+        break;
+
+      case 8:
+        Crusader_Craft.saveGame();
+        break;
+
+      case 9:
+        Crusader_Craft.loadGame();
         break;
 
       default:
